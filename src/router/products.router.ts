a3a177d12d6315adce6292e08e7b701c2f31c5b4
Elena <ebromeo88@gmail.com>
2023-02-25 21:43:41 +0100

    Add product-router file

---
+++ src/router/products.router.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { ProductsController } from '../controllers/products.controller';
+import { ProductsRepo } from '../repository/products.repo';
+
+// eslint-disable-next-line new-cap
+export const productsRouter = Router();
+const repo = new ProductsRepo();
+const controller = new ProductsController(repo);
+
+productsRouter.get('/', controller.getAll.bind(controller));
+productsRouter.get('/:id', controller.get.bind(controller));
+productsRouter.post('/', controller.post.bind(controller));
+productsRouter.patch('/:id', controller.patch.bind(controller));
+productsRouter.delete('/:id', controller.delete.bind(controller));
