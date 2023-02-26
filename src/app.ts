37b69eb8e3136aa3b0b317daa7c9040f61446b9b
Elena <ebromeo88@gmail.com>
2023-02-26 15:50:47 +0100

    Modify app file

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import cors from 'cors';
 import { productsRouter } from './router/products.router.js';
 
 export const app = express();
-app.disable('x-powered-by');
 
 const corsOptions = {
   origin: '*',
@@ -13,10 +12,6 @@ const corsOptions = {
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors(corsOptions));
-
-app.use((_req, _resp, next) => {
-  console.log('Hola Mundo');
-  next();
-});
+app.disable('x-powered-by');
 
 app.use('/products', productsRouter);
